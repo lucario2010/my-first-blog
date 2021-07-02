b5523c7084462cb17a24806b19989d80a310d459
lucario2010 <throwaway332020+lucario.git@gmail.com>
2021-07-02 13:42:18 +1000

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,13 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Australia/ACT'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
